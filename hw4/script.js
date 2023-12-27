1f32d6035740723782b535aceb80386d9cc7a214
Dmytro Puchkov <puchkov.dev@gmail.com>
2023-12-27 13:08:28 +0200

    create homework 4

---
+++ hw4/script.js
@@ -0,0 +1,30 @@
+// 1 Розробити скрипт який буде запитувати ім’я користувача (userName) за допомогою prompt та вивести результат за допомогою alert
+
+// 1.1 перевірити змінну userName що вона не порожня, якщо порожня то за замовчуванням записати значення “anonym”. Відобразити значення змінної userName.
+
+let userName = prompt('Enter your name', '');
+userName = userName || 'anonym';
+alert(`Hello ${userName}!`);
+
+// 1.2 Якщо користувач ввів “admin”, то відобразити - “Hello admin”, якщо щось інше, то “Welcome”
+
+userName = prompt('Enter your name', '');
+alert(userName === 'admin' && 'Hello admin' || 'Welcome');
+
+// 2 Розробити скрипт який отримає від юзера(prompt) дані login, password, age
+
+const login = prompt('Enter login');
+const password = prompt('Enter password');
+const age = prompt('Enter age');
+
+// 2.1. Перевірити що login та password обидва поля не пусті, якщо хочаб одне пусте вивести повідомлення, “ login or password incorrect”, якщо всі заповнені то ‘welcome’
+
+alert((login === '' || password === '') && 'Login or password incorrect' || 'Welcome');
+
+// 2.2 Дізнатися які саме поля було не заповнено та вивести підказку за допомогою alert()
+
+ alert((login === '' && 'Login is empty' ) || (password === '' && 'Password is empty') || 'Login and password correct');
+
+// 2.3. Перевірити одним виразом що змінна age заповнена та вік користувача від 18 до 28, якщо все добре то вивести повідомлення Hello, інакше Goobay
+
+alert((age != '' && age > 17 && age < 29) && 'Hello' || 'Goodbay');
