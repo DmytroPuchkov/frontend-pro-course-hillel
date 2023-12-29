1288424bace5f76d204ecb9d04b82faa3928b093
Dmytro Puchkov <puchkov.dev@gmail.com>
2023-12-29 20:37:00 +0200

    update homework 5

--- hw5/script.js
+++ hw5/script.js
@@ -14,3 +14,21 @@ switch (true) {
   default:
     console.log('OK!');
 }
+
+// v2
+
+// switch (numOrStr) {
+//   case null:
+//     console.log('ви скасували');
+//     break;
+//   case '':
+//     console.log('Empty String');
+//     break;
+//   default:
+//     if (isNaN(+numOrStr)) {
+//       console.log('number is Ba_NaN');
+//     } else {
+//       console.log('OK!');
+//     }
+//     break;
+// }
