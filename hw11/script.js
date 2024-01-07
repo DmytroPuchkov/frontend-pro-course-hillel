cb101ad2108e90da52ed023d69693b1bf1ab7373
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-01-07 22:58:52 +0200

    create homework 11

---
+++ hw11/script.js
@@ -0,0 +1,13 @@
+const arr = [1, 5, 6, 2, 4];
+
+console.log('Масив:', arr);
+
+for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+console.log('Перевернутий масив:', arr);
+
+
