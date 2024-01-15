b6b8c651c19a3eb8c037f461456d33d3bb7919f8
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-01-15 23:10:19 +0200

    create homework 14

---
+++ hw14/script.js
@@ -0,0 +1,16 @@
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+    }
+  }
+  
+  return array;
+};
+
+removeElement(array, 5);
+
+console.log(array);
+
