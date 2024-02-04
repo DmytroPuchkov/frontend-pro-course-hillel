4709bd2485e22a39d1f466cb5124a5f44839eefa
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-02-04 18:39:34 +0200

    create homework 24

---
+++ hw24/script.js
@@ -0,0 +1,40 @@
+// Завдання 1
+
+const obj = {
+  car: "BMW",
+  type: "sedan",
+  color: "black",
+  renderObject: function () {
+    for (let key in this) {
+      if (key !== 'renderObject' && typeof this[key] !== 'function') {
+        document.write(`<div>${key}: ${this[key]}</div>`);
+      }
+    }
+  }
+};
+
+obj.renderObject();
+
+// Завдання 2
+
+data = {
+  addRecord: function () {
+    for (let i = 0; i < arguments.length; i++) {
+      const record = arguments[i];
+      for (let key in record) {
+        this[key] = record[key];
+      }
+    }
+  },
+  p: 600,
+  str: 'hello',
+  y: -50
+}
+
+data.addRecord({ x: 10 }, { y: 20 }, { z: 30, x: 50 });
+
+console.log(data.x); // 50
+console.log(data.y); // 20
+console.log(data.z); // 30
+console.log(data.p); // 600
+console.log(data.str); // 'hello'
