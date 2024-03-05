8655cecf19ede8f88917b66594590b7cf8686742
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-03-06 00:21:14 +0200

    update hw34

--- hw34/script.js
+++ hw34/script.js
@@ -1,18 +1,24 @@
 let number = 1234325153444;
 
-const array = Array.from(String(number), Number);
+const setNumberToArray = (number) => Array.from(String(number), Number);
 
-const countNumbers = array.reduce((acc, num) => {
-  acc[num] = acc[num] ? acc[num] + 1 : 1;
-  return acc;
-}, {});
+const getCountNumbers = (array) =>
+  array.reduce((acc, num) => {
+    acc[num] = acc[num] ? acc[num] + 1 : 1;
+    return acc;
+  }, {});
 
-const luckyNumbers = Object.keys(countNumbers).reduce((acc, num) => {
-  if (countNumbers[num] === Number(num)) {
-    acc.push(num);
-  }
-  return acc;
-}, []);
+const getLuckyNumbers = (countNumbers) =>
+  Object.keys(countNumbers).reduce((acc, num) => {
+    if (countNumbers[num] === Number(num)) {
+      acc.push(num);
+    }
+    return acc;
+  }, []);
 
-console.log(countNumbers);
-console.log(luckyNumbers);
+const array = setNumberToArray(number);
+const countNumbers = getCountNumbers(array);
+const luckyNumbers = getLuckyNumbers(countNumbers);
+
+console.log("Підрахунок чисел:", countNumbers);
+console.log("Щасливі числа:", luckyNumbers);
