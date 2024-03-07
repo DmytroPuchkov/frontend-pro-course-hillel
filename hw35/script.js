e0860db646f5d214fbfb7ed4b023a36587c09fcc
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-03-07 18:28:45 +0200

    create new tip

--- hw35/script.js
+++ hw35/script.js
@@ -9,3 +9,20 @@ function addFunc(a, b) {
 const add = (a, b) => a + b;
 
 console.log(add(4, 8));
+
+// Arra.from()
+
+const str = "Chipi chipi chapa chapa"
+const arr = Array.from(str);
+
+console.log(arr);
+
+// Display Data with console.table()
+
+const hero1 = {
+  heroName: 'Kai',
+  heroClass: 'Mag',
+  health: 100,
+}
+
+console.table(hero1);
