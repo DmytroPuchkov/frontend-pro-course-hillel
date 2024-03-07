cfa278b97d92d625061341d9a85477844d0373d2
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-03-07 18:35:22 +0200

    update tips

--- hw35/script.js
+++ hw35/script.js
@@ -9,3 +9,16 @@ function addFunc(a, b) {
 const add = (a, b) => a + b;
 
 console.log(add(4, 8));
+
+// Object.seal()
+
+const person2 = {
+  name: 'John',
+  age: 25
+}
+
+Object.seal(person2);
+
+person.profession = "Programmer";
+
+console.log(person2);
