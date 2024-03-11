10b4d99e57704b0d56a91a0d01280459f755028d
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-03-11 23:51:35 +0200

    create release2

--- hw35/script.js
+++ hw35/script.js
@@ -27,3 +27,10 @@ const doubled8 = numbers8.map(num => num * 2);
 
 console.log(numbers8); // Output: [1, 2, 3, 4]
 console.log(doubled8); // Output: [2, 4, 6, 8]
+
+// Remove Array Duplicates
+
+const arrWithDuplicates = [1, 12, 2, 13, 4, 4, 13];
+const arrWithoutDuplicates = [...new Set(arrWithDuplicates)];
+
+console.log(arrWithoutDuplicates); // Output: [1, 12, 2, 13, 4]
