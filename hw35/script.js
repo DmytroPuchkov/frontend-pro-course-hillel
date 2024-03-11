31ec3de36a3a8f45fbbaeb86e4189d3112c75cbe
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-03-11 23:10:03 +0200

    update f2-9

--- hw35/script.js
+++ hw35/script.js
@@ -27,3 +27,16 @@ const doubled3 = numbers3.map(num => num * 2);
 
 console.log(numbers3); // Output: [1, 2, 3, 4]
 console.log(doubled3); // Output: [2, 4, 6, 8]
+
+// Object.seal()
+
+const person5 = {
+  name: 'John', 
+  age: 25
+};
+
+Object.seal(person5);
+
+person.profession = "Programmer";
+
+console.log(person5); // Output: {name: 'John', age: 25}
