492824cc1fbcfae7ebe4a8587806ecd384d31968
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-03-16 20:16:57 +0200

    update hw37

--- hw37/script.js
+++ hw37/script.js
@@ -1,10 +1,6 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
-
-  this.showInfo = function() {
-    console.log(`Їмʼя: ${this.name}, вік: ${this.age}.`);
-  };
 }
 
 function Car(brand, model, year, number) {
@@ -12,35 +8,42 @@ function Car(brand, model, year, number) {
   this.model = model;
   this.year = year;
   this.number = number;
+}
 
-  this.setOwner = function(person) {
-    if (person.age >= 18) {
-      this.owner = person;
-    } else {
-      console.log(`${person.name} ще немає 18 років.`);
-    }
-  };
+Person.prototype.showInfo = function() {
+  console.log(`Їмʼя: ${this.name}, вік: ${this.age}.`);
+}
 
-  this.showInfo = function() {
-    console.log(`Бренд: ${this.brand}, модель: ${this.model}, рік випуску: ${this.year}, номер: ${this.number}.`);
-    if (this.owner) {
-      this.owner.showInfo();
-    } else {
-      console.log("Автівка немає власника.");
-    }
+Car.prototype.setOwner = function(person) {
+  if (person.age >= 18) {
+    this.owner = person;
+  } else {
+    console.log(`${person.name} ще немає 18 років.`);
   }
 }
 
-console.log('Випадок 1');
+Car.prototype.showInfo = function() {
+  console.log(`Бренд: ${this.brand}, модель: ${this.model}, рік випуску: ${this.year}, номер: ${this.number}.`);
+  if (this.owner) {
+    this.owner.showInfo();
+  } else {
+    console.log("Автівка немає власника.");
+  }
+}
+
+console.log('%cВипадок 1','color: yellow; background-color: blue;');
 
 const person1 = new Person("Сергій", 17);
 const car1 = new Car("BMW", "M3", 2017, "АА7777ХХ");
+
+person1.showInfo();
 car1.setOwner(person1);
 car1.showInfo();
 
-console.log('Випадок 2');
+console.log('%cВипадок 2','color: yellow; background-color: blue;');
 
 const person2 = new Person("Надія", 24);
 const car2 = new Car("Dodge", "Viper", 2008, "КА1001АН");
+
 car2.setOwner(person2);
 car2.showInfo();
