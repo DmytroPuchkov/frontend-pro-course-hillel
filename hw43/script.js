feecaf8021de999d619089cbd34aac691e223a4b
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-04-01 18:07:14 +0300

    update hw43

--- hw43/script.js
+++ hw43/script.js
@@ -1,17 +1,17 @@
 function updateTextarea() {
-  const input1 = document.getElementById('input1').value;
-  const input2 = document.getElementById('input2').value;
-  const input3 = document.getElementById('input3').value;
+  const inputs = [...document.querySelectorAll('#input1, #input2, #input3')];
+  const output = document.getElementById('output');
+    
+  return () => {
+    const currentValue = inputs.map(input => input.value).join(', ');
+    const previousValue = output.value;
 
-  const currentValue = input1 + ', ' + input2 + ', ' + input3;
-
-  const previousValue = document.getElementById('output').value;
-
-  if (currentValue !== previousValue) {
-    document.getElementById('output').value = currentValue;
+    if (currentValue !== previousValue) {
+        output.value = currentValue;
+    }   
   }
 }
 
 const N = 2000;
 
-setInterval(updateTextarea, N);
+setInterval(updateTextarea(), N);
