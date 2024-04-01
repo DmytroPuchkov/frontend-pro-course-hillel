3f21ecd50a51d0e212e30bd027d114276eca7c81
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-04-01 18:52:02 +0300

    update hw44

--- hw44/script.js
+++ hw44/script.js
@@ -4,24 +4,34 @@ const greenLine = document.getElementById('green');
 const redLine = document.getElementById('red');
 
 function calculateCommission(value) {
-  const rate = value < 20 ? 0.02 : value < 50 ? 0.04 : value < 75 ? 0.06 : 0.08;
+  if (value < 20) return value * 0.02;
+  if (value < 50) return value * 0.04;
+  if (value < 75) return value * 0.06;
 
-  return value * rate;
+  return value * 0.08;
+}
+
+function update(target, managed) {
+  managed.value = target.value;
 }
 
 function updateDiagram(value) {
   const intValue = parseInt(value, 10);
   const commissionValue = calculateCommission(intValue);
 
-  input.value = intValue;
-  slider.value = intValue;
   greenLine.style.height = `${intValue}px`;
-
-  redLine.style.height = `${commissionValue}px`;
   redLine.style.bottom = `${intValue}px`;
+  redLine.style.height = `${commissionValue}px`;
 }
 
-slider.addEventListener('input', () => updateDiagram(slider.value));
-input.addEventListener('input', () => updateDiagram(input.value));
+slider.addEventListener('input', () => {
+  update(slider, input);
+  updateDiagram(slider.value);
+});
+
+input.addEventListener('input', () => {
+  update(input, slider);
+  updateDiagram(input.value);
+});
 
 updateDiagram(slider.value);
