2b0488d9f4be738e8f749d49ce431d3ab0f7a092
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-05-09 16:00:39 +0300

    upd hw51

--- hw51/script.js
+++ hw51/script.js
@@ -1,45 +1,36 @@
 const apiUrl = "http://api.openweathermap.org/data/2.5/weather";
 const apiKey = "5d066958a60d315387d9492393935c19";
 const city = "Kyiv";
-const lang = "UA";
+const lang = "ua";
 
-function sendAjax({ url, method = "GET", data = null, success, error }) {
-  const xhr = new XMLHttpRequest();
-
-  xhr.onload = () => {
-    const response = xhr.responseText;
-
-    if (typeof response === "string") {
-      success(JSON.parse(response));
-      return;
-    }
-
-    success(response);
-  };
-
-  xhr.onerror = error;
-
-  xhr.open(method, url);
-  method === "GET" ? xhr.send() : xhr.send(data);
+function getUrl(city, lang, apiKey) {
+  return `${apiUrl}?q=${city}&units=metric&lang=${lang}&APPID=${apiKey}`;
 }
 
-sendAjax({
-  url: `${apiUrl}?q=${city}&units=metric&lang=${lang}&APPID=${apiKey}`,
-  success: (data) => {
-    document.getElementById("city").textContent = data.name;
-    document.getElementById("temp").textContent = data.main.temp;
-    document.getElementById("pressure").textContent = data.main.pressure;
-    document.getElementById("description").textContent = data.weather[0].description;
-    document.getElementById("humidity").textContent = data.main.humidity;
-    document.getElementById("speed").textContent = data.wind.speed;
-    document.getElementById("deg").textContent = data.wind.deg;
+function getWeather(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(getInterface)
+    .catch(getError);
+}
 
-    const iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+function getInterface(data) {
+  document.getElementById("city").textContent = data.name;
+  document.getElementById("temp").textContent = data.main.temp;
+  document.getElementById("pressure").textContent = data.main.pressure;
+  document.getElementById("description").textContent = data.weather[0].description;
+  document.getElementById("humidity").textContent = data.main.humidity;
+  document.getElementById("speed").textContent = data.wind.speed;
+  document.getElementById("deg").textContent = data.wind.deg;
+  document.getElementById("icon").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+}
 
-    document.getElementById('icon').src = iconUrl;
-  },
-  error: () => {
-    console.error('Error');
-  }
+function getError(error) {
+  console.error("Error:", error);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const weatherUrl = getUrl(city, lang, apiKey);
+
+  getWeather(weatherUrl);
 });
-
